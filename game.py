2f4fdf255e37a9ba1d63e2de5b7d496e56ebf2ec
bwoo14 <bwoo14@my.bcit.ca>
2022-11-21 22:22:18 -0800

    added main menu screen and pause menu, not yet functional

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from models.character import Character
 from global_variables import *
 from models.platform import Platform
 from models.world import World
+import math
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -13,27 +14,45 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('South Park Game')
 
 # Define Game Variables
-
+tiles = math.ceil((SCREEN_WIDTH) / SCREEN_WIDTH) + 1
 
 # Load Images
 
 platform = Platform()
 world = World()
+
 character = Character('Cartman', 480, 270)
 
-run = True
-while run:
+def play_game():
+    run = True
+    while run:
+        clock.tick(FPS)
+        world.draw(screen, tiles)
+        character.update(screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.display.update()
 
-    clock.tick(FPS)
-    world.draw(screen)
-    platform.draw(screen)
+def pause():
+    pass
 
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+def main_menu():
+    menu_image = pygame.image.load('images/main-menu.png')
+    menu_image = pygame.transform.scale(menu_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        screen.blit(menu_image, (0, 0))
+        pygame.display.update()
 
-    character.update(screen)
-    pygame.display.update()
+
+
+
+main_menu()
+
 
 pygame.quit()
---
+++ models/button.py
@@ -0,0 +1,3 @@
+import pygame
+from pygame.locals import *
+from global_variables import *
