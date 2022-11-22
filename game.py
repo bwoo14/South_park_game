efb3a5ed1e77eae253739f769c124a6ae0b83bbb
bwoo14 <bwoo14@my.bcit.ca>
2022-11-21 18:34:28 -0800

    First init
    Game already has background and movable character (Eric Cartman) that can jump. Also has animation for moving

---
+++ game.py
@@ -0,0 +1,37 @@
+import pygame
+from pygame.locals import *
+from models.character import Character
+from global_variables import *
+pygame.init()
+
+clock = pygame.time.Clock()
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('South Park Game')
+
+# Define Game Variables
+
+
+# Load Images
+
+bg_img = pygame.image.load('images/background.jpg')
+bg_img = pygame.transform.scale(bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
+platform = pygame.image.load('images/platform.png')
+
+character = Character('Cartman', 480, 270)
+
+run = True
+while run:
+
+    clock.tick(FPS)
+    screen.blit(bg_img, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    character.update(screen)
+    pygame.display.update()
+
+pygame.quit()
---
+++ models/character.py
@@ -0,0 +1,109 @@
+import pygame
+from pygame.locals import *
+from global_variables import *
+
+import pygame
+
+DEFAULT_IMAGE_SIZE = (100, 100)
+walk_cooldown = 10
+class Character:
+    def __init__(self, character, x, y):
+        if character == 'Cartman':
+            self.images_right = []
+            self.images_left = []
+            self.index = 1
+            self.counter = 0
+
+            # Getting images for guy walking right
+            for num in range(1, 5):
+                img_right = pygame.image.load(f'images/cartmanside{num}.png')
+                img_right = pygame.transform.scale(img_right, DEFAULT_IMAGE_SIZE)
+                img_right = pygame.transform.flip(img_right, True, False)
+                self.images_right.append(img_right)
+            
+            # Getting images for guy walking left
+            for num in range(1, 5):
+                img_left = pygame.image.load(f'images/cartmanside{num}.png')
+                img_left = pygame.transform.scale(img_left, DEFAULT_IMAGE_SIZE)
+                self.images_left.append(img_left)
+
+            self.image = self.images_right[self.index]
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+            self.vel_y = 0
+            self.jumped = False
+            self.direction = 0
+    
+    def update(self, screen):
+
+        dx = 0
+        dy = 0
+        
+        # Get key presses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -25
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+            self.counter += 1
+            self.direction = -1
+        if key[pygame.K_RIGHT]:
+            dx += 5
+            self.counter += 1
+            self.direction = 1
+        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+            # If neither keys are pushed, put character back to starting image.
+            self.counter = 0
+            self.index = 1
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+
+        # Handle animation
+        
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+        # add gravity
+        
+        self.vel_y += 2
+        if self.vel_y > 15:
+            self.vel_y = 15
+        dy += self.vel_y
+        # Check for collision
+
+        # Update player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # Don't let player go below screen
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+            dy = 0
+        # Don't let player to left of screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        
+        # Don't let player to right of screen
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+
+        # Don't let player go above screen
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+        screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
