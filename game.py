582b9360a58913d7d3ee94c3aa6e3434f499ad19
bwoo14 <bwoo14@my.bcit.ca>
2022-11-21 23:18:32 -0800

    added logo

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from models.character import Character
 from global_variables import *
 from models.platform import Platform
 from models.world import World
+from models.button import Button
 import math
 pygame.init()
 
@@ -40,13 +41,20 @@ def pause():
 def main_menu():
     menu_image = pygame.image.load('images/main-menu.png')
     menu_image = pygame.transform.scale(menu_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    logo = pygame.image.load('images/logo.png').convert()
+    logo_rect = logo.get_rect()
+    logo_rect.center = (SCREEN_WIDTH/2, 75)
+    
+
     run = True
     while run:
         clock.tick(FPS)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         screen.blit(menu_image, (0, 0))
+        screen.blit(logo, logo_rect)
         pygame.display.update()
 
 
